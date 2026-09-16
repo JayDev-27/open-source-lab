ae6406e683b8dc8f15b84c995a81506634bc014b
Jatin Pandey <jatinjio1212@gmail.com>
2026-09-17 02:14:32 +0530

    feat(strings): implement CamelCase to Snake_Case
    Co-authored-by: JayDev-27 <jtnpanjio@gmail.com>

---
+++ src/strings/formatting/camel_to_snake.py
@@ -0,0 +1,5 @@
+"""Transforms camelCase to snake_case."""
+import re
+def camel_to_snake(name: str) -> str:
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
