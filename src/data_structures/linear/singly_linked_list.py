fddc3f5378b37f5074e7cb98c5b1926185c9e77a
Jatin Pandey <jatinjio1212@gmail.com>
2026-09-17 02:07:26 +0530

    feat(data-structures): implement Singly Linked List
    Co-authored-by: JayDev-27 <jtnpanjio@gmail.com>

---
+++ src/data_structures/linear/singly_linked_list.py
@@ -0,0 +1,18 @@
+"""Singly Linked List class."""
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        curr = self.head
+        while curr.next:
+            curr = curr.next
+        curr.next = new_node
